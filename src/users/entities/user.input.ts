04622ec10462faa5f910a5e065178737d1620f8a
cesar-1 <csantana2800@outlook.com>
2026-01-07 16:42:27 -0500

    :wq :q!

--- src/users/entities/user.input.ts
+++ src/users/entities/user.input.ts
@@ -1,6 +1,22 @@
-import { InputType } from "@nestjs/graphql";
+import { Field, InputType } from "@nestjs/graphql";
 
 @InputType()
 export class UserInput {
+    @Field({ nullable: false})
+    id: string;
 
+    @Field({ nullable: false})
+    name: string;   
+    @Field({ nullable: false})
+    email: string;  
+    @Field({ nullable: true})
+    password_hash: string;
+
+    @Field({ nullable: false})
+    role: 'USER' | 'ADMIN' | 'AGENT';     
+    @Field({ nullable: false})
+    is_active: boolean; 
+    
+    @Field({ nullable: false})
+    created_at: Date;
 }
--- src/users/interfaces/user/user.interface.ts
+++ src/users/interfaces/user/user.interface.ts
@@ -1,1 +1,10 @@
-export interface User {}
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  password_hash: string;
+  role: 'USER' | 'ADMIN' | 'AGENT';
+  is_active: boolean;
+  created_at: Date;
+  updated_at: Date;
+}
